5e1d25ce0d80f72919f4709a82d2d63cb4d88026
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-04-14 13:32:31 -0300

    validacoes para campos

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,13 @@
 
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true
 end
+
+
+# para fazer rollback e atualizar executar a nova migração
+# rails db:migrate:redo
+# isso evita justamente ter que rodar o
+# `rails db:rollback`
+# `rails db:migrate`
