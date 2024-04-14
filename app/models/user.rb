ea8821d0ec45ff761c0511420dae7477342c250c
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-04-14 13:43:15 -0300

    mais validacoes e exemplo

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@
 # password
 # password_confirmation
 
+# Para usar voce pode fazer
+# User.create(email: 'email@test.com', password: '1234', password_confirmation: '1234')
+
 class User < ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Must be a valid email address' }
 end
 
 
