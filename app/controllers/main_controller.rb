4485e02f355faa241f70faecbcd26d7cae8ef50c
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-04-14 15:43:09 -0300

    adicionando session cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,9 @@
 class MainController < ApplicationController
   def index
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+    end
+
     @content = 'Root controller'
     # flash.now[:notice] = 'Logged'
     # flash.now[:alert] = 'You not logged'
