313196d175da678154b6661f7a6fd4c600994f66
Giovanny Cordeiro <giovannycordeiropb@gmail.com>
2024-04-15 05:33:49 -0300

    editando senha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :set_current_user
 
+  def require_user_logged_id
+    redirect_to sign_in_path, alert: 'you must be signed!' if Current.user.nil?
+  end
+
   def set_current_user
     if session[:user_id]
       Current.user = User.find_by(id: session[:user_id])
